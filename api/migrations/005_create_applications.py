4c4be1dfe0e93b81d8b59a5ef4cd4c9c810070f6
MPyarali <monika.pyarali@gmail.com>
2024-04-17 15:49:14 -0700

    Updates table with status

--- api/migrations/005_create_applications.py
+++ api/migrations/005_create_applications.py
@@ -6,13 +6,14 @@ steps = [
             id SERIAL PRIMARY KEY NOT NULL,
             tournament_id INT NOT NULL,
             user_id INT NOT NULL,
+            status VARCHAR(20) NOT NULL DEFAULT 'pending', 
             FOREIGN KEY(tournament_id) REFERENCES tournaments(id),
             FOREIGN KEY(user_id) REFERENCES users(id)
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE tournaments;
+        DROP TABLE applications;
         """,
     ],
 ]
