d09304acbfc2d9faca536107728a47f1a3bbec35
Chase Robles <chase.robles@live.com>
2024-04-24 16:59:46 -0500

    L

--- api/migrations/001_create_users.py
+++ api/migrations/001_create_users.py
@@ -11,7 +11,9 @@ steps = [
             last_name VARCHAR(100) NOT NULL,
             photo_url VARCHAR(256) NOT NULL,
             phone_number VARCHAR(100) NOT NULL,
-            address VARCHAR(100) NOT NULL
+            address VARCHAR(100) NOT NULL,
+            CHECK (LENGTH(username) >= 1),
+            CHECK (LENGTH(password) >= 1)
         );
         """,
         # "Down" SQL statement
