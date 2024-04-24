a83a40fe08359c783d879cd0df51445cdf8cf158
ChaseRobles <chase.robles@live.com>
2024-04-24 21:05:36 +0000

    Tournament backend

--- api/migrations/004_create_tournament.py
+++ api/migrations/004_create_tournament.py
@@ -4,6 +4,7 @@ steps = [
         """
         CREATE TABLE tournaments (
             id SERIAL PRIMARY KEY NOT NULL,
+            user_id INT NOT NULL DEFAULT 0,
             event_name VARCHAR(100) NOT NULL,
             roster_size SMALLINT NOT NULL,
             event_start DATE NOT NULL,
@@ -12,7 +13,8 @@ steps = [
             picture_url VARCHAR(100) NOT NULL,
             entry_fee FLOAT NOT NULL,
             prize FLOAT NOT NULL,
-            sponsors VARCHAR(100) NOT NULL
+            sponsors VARCHAR(100) NOT NULL,
+            FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
         );
         """,
         # "Down" SQL statement
