30576cb67aff2cbdb584d019a6ca62baaab97a37
Elliott Porter <elliottporterhackreactor@gmail.com>
2024-05-01 16:14:50 -0600

    okay, actually done

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,36 +1,36 @@
-import './App.css';
-import './index.css';
+import './App.css'
+import './index.css'
 
-const API_HOST = import.meta.env.VITE_API_HOST;
+const API_HOST = import.meta.env.VITE_API_HOST
 
 if (!API_HOST) {
-    throw new Error('VITE_API_HOST is not defined');
+    throw new Error('VITE_API_HOST is not defined')
 }
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import useAuthService from './hooks/useAuthService';
-import gamesterImage from './images/gamesterImage.png';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import useAuthService from './hooks/useAuthService'
+import gamesterImage from './images/gamesterImage.png'
 
-import ApplicationStatusDropdown from './components/TournamentApplicationUpdate';
-import CompetitorDetails from './components/CompetitorDetails';
-import LandingPage from './components/LandingPage';
-import MyCompetitorTournaments from './components/MyCompetitorTournaments';
-import SignInForm from './components/SignInForm';
-import SignOut from './components/SignOut';
-import SignUpForm from './components/SignUpForm';
-import TournamentApplicationList from './components/TournamentApplications';
-import TournamentDetails from './components/TournamentDetails';
-import TournamentList from './components/Tournaments';
-import TournamentManagerDetails from './components/TournamentManagerDetails';
-import UpcomingTournaments from './components/UpcomingTournaments';
-import ProfileConstruct from './components/ProfileConstruct';
+import ApplicationStatusDropdown from './components/TournamentApplicationUpdate'
+import CompetitorDetails from './components/CompetitorDetails'
+import LandingPage from './components/LandingPage'
+import MyCompetitorTournaments from './components/MyCompetitorTournaments'
+import SignInForm from './components/SignInForm'
+import SignOut from './components/SignOut'
+import SignUpForm from './components/SignUpForm'
+import TournamentApplicationList from './components/TournamentApplications'
+import TournamentDetails from './components/TournamentDetails'
+import TournamentList from './components/Tournaments'
+import TournamentManagerDetails from './components/TournamentManagerDetails'
+import UpcomingTournaments from './components/UpcomingTournaments'
+import ProfileConstruct from './components/ProfileConstruct'
 import TournamentCreateForm from './components/TournamentCreateForm'
-import TournamentDetailsEdit from './components/TournamentDetailsEdit';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+import TournamentDetailsEdit from './components/TournamentDetailsEdit'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
-    const { user } = useAuthService();
+    const { user } = useAuthService()
     return (
         <BrowserRouter>
             <NavBar />
@@ -92,7 +92,7 @@ function App() {
             </div>
             <Footer />
         </BrowserRouter>
-    );
+    )
 }
 
-export default App;
+export default App
